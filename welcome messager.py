8b88fc9eef75aab688e346b2a27f9f488fc00905
manasatm24 <mmallikarjuntm@gmail.com>
2026-04-18 23:19:01 +0530

    Create welcome messager.py

---
+++ welcome messager.py
@@ -0,0 +1,7 @@
+name=input("Enter your name: ")
+hobby=input("Enter your hobby: ")
+print("\n---Welcome Message---")
+print(f"Hello {name}")
+print("Welcome to the world of python programming!")
+print(f"It's great to know that you love {hobby}.")
+print("Get ready to build something amazing today.")
